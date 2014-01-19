7481f239796fe9b8da204f7f47594557f69b7a66
bsmitty <bsmitty83@yahoo.com>
2014-01-18 20:28:16 -0500

    Revert "changed gpu oc sysfs to correspond to new freq"
    This reverts commit 10ccef888d9163ea93ce5fa405431918d4ffd4e2.

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -506,7 +506,7 @@ static ssize_t store_gpu_oc(struct cpufreq_policy *policy, const char *buf, size
 {
 	int prev_oc, ret1, ret2; 
         struct device *dev;
-	unsigned long gpu_freqs[3] = {384000000,460800000,537600000};
+	unsigned long gpu_freqs[3] = {307200000,384000000,512000000};
 
 	prev_oc = oc_val;
 	if (prev_oc < 0 || prev_oc > 2) {
