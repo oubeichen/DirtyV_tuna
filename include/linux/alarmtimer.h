6dfda3120797937fdf10ffd3092cc0cca1b77bd0
bsmitty <bsmitty83@yahoo.com>
2014-01-21 10:11:51 -0500

    Merge branch 'timers-core-for-linus' of git://git.kernel.org/pub/scm/…
    …linux/kernel/git/tip/tip
    
    * 'timers-core-for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip: (23 commits)
      time, s390: Get rid of compile warning
      dw_apb_timer: constify clocksource name
      time: Cleanup old CONFIG_GENERIC_TIME references that snuck in
      time: Change jiffies_to_clock_t() argument type to unsigned long
      alarmtimers: Fix error handling
      clocksource: Make watchdog reset lockless
      posix-cpu-timers: Cure SMP accounting oddities
      s390: Use direct ktime path for s390 clockevent device
      clockevents: Add direct ktime programming function
      clockevents: Make minimum delay adjustments configurable
      nohz: Remove "Switched to NOHz mode" debugging messages
      proc: Consider NO_HZ when printing idle and iowait times
      nohz: Make idle/iowait counter update conditional
      nohz: Fix update_ts_time_stat idle accounting
      cputime: Clean up cputime_to_usecs and usecs_to_cputime macros
      alarmtimers: Rework RTC device selection using class interface
      alarmtimers: Add try_to_cancel functionality
      alarmtimers: Add more refined alarm state tracking
      alarmtimers: Remove period from alarm structure
      alarmtimers: Remove interval cap limit hack
      ...

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -11,7 +11,16 @@ enum alarmtimer_type {
 	ALARM_BOOTTIME,
 
 	ALARM_NUMTYPE,
+  };
+
+enum alarmtimer_restart {
+  ALARMTIMER_NORESTART,
+  ALARMTIMER_RESTART,
 };
+
+#define ALARMTIMER_STATE_INACTIVE  0x00
+#define ALARMTIMER_STATE_ENQUEUED  0x01
+#define ALARMTIMER_STATE_CALLBACK  0x02
 
 /**
  * struct alarm - Alarm timer structure
@@ -24,11 +33,10 @@ enum alarmtimer_type {
  * @data:	Internal data value.
  */
 struct alarm {
-	struct timerqueue_node	node;
-	ktime_t			period;
+	enum alarmtimer_restart  (*function)(struct alarm *, ktime_t now);
 	void			(*function)(struct alarm *);
 	enum alarmtimer_type	type;
-	bool			enabled;
+	int      		state;
 	void			*data;
 };
 
@@ -36,5 +44,37 @@ void alarm_init(struct alarm *alarm, enum alarmtimer_type type,
 		void (*function)(struct alarm *));
 void alarm_start(struct alarm *alarm, ktime_t start, ktime_t period);
 void alarm_cancel(struct alarm *alarm);
+    enum alarmtimer_restart (*function)(struct alarm *, ktime_t));
+void alarm_start(struct alarm *alarm, ktime_t start);
+int alarm_try_to_cancel(struct alarm *alarm);
+int alarm_cancel(struct alarm *alarm);
+
+u64 alarm_forward(struct alarm *alarm, ktime_t now, ktime_t interval);
+
+/*
+ * A alarmtimer is active, when it is enqueued into timerqueue or the
+ * callback function is running.
+ */
+static inline int alarmtimer_active(const struct alarm *timer)
+{
+  return timer->state != ALARMTIMER_STATE_INACTIVE;
+}
+
+/*
+ * Helper function to check, whether the timer is on one of the queues
+ */
+static inline int alarmtimer_is_queued(struct alarm *timer)
+{
+  return timer->state & ALARMTIMER_STATE_ENQUEUED;
+}
+
+/*
+ * Helper function to check, whether the timer is running the callback
+ * function
+ */
+static inline int alarmtimer_callback_running(struct alarm *timer)
+{
+  return timer->state & ALARMTIMER_STATE_CALLBACK;
+}
 
 #endif
