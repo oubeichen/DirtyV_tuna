56046349ce5e9ac2e94050061a15634ac64137e3
oubeichen <oubeichen@gmail.com>
2014-01-26 20:36:13 +0800

    My purple tint fix. Users report 80-100% purple tint (at low brightness) cure. Reports of increase in max brightness also.
    Conflicts:
    	arch/arm/mach-omap2/board-tuna-display.c

--- arch/arm/mach-omap2/board-tuna-display.c
+++ arch/arm/mach-omap2/board-tuna-display.c
@@ -747,9 +747,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_8500k = {
 	},
 	.color_adj = {
 		.mult = {
-			1900000000U,
-			1950000000U,
-			2150000000U,
+			2163736680U,
+			1976041377U,
+			1614370595U,
 		},
 		.rshift = 31,
 	},
@@ -795,9 +795,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_6500k = {
 	},
 	.color_adj = {
 		.mult = {
-			1900000000U,
-			1950000000U,
-			2150000000U,
+			2004318071U,
+			2004318071U,
+			2004318071U,
 		},
 		.rshift = 31,
 	},
@@ -843,9 +843,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_sm2a2 = {
 	},
 	.color_adj = {
 		.mult = {
-			1900000000U,
-			1950000000U,
-			2150000000U,
+			2004318071U,
+			2004318071U,
+			2004318071U,
 		},
 		.rshift = 31,
 	},
